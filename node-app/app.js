fe8379e61bab750dd539abfb024fc761ed08f3e3
Jamillah Bello <jaybills369@gmail.com>
2025-04-07 13:04:52 +0100

    sample apps
    Signed-off-by: Jamillah Bello <jaybills369@gmail.com>

---
+++ node-app/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Node.js app with shared DB is up and running!');
+});
+
+app.listen(3000, () => {
+    console.log('Node.js app listening on port 3000!');
+});
