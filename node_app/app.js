e76815ac5ad75c1440408bc87689fcbbb20fbca7
Jay <105195327+StrangeJay@users.noreply.github.com>
2025-04-09 10:43:18 +0100

    Update app.js

--- node_app/app.js
+++ node_app/app.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Node.js app with shared DB is up and running!');
 });
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
     console.log('Node.js app listening on port 3000!');
 });
